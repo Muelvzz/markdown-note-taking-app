8218013939f158fb75c3c7e68da202656c4bc7f3
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-04-26 17:41:59 +0800

    Removed tracked files that should be ignored

--- backend/app/users.py
+++ backend/app/users.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
+from schemas import Upload
 
 router = APIRouter()
 
-@router.get("/file", tags=["file upload"])
+@router.post("/file")
 async def read_file():
     return {
         "message": "This is a router path"
